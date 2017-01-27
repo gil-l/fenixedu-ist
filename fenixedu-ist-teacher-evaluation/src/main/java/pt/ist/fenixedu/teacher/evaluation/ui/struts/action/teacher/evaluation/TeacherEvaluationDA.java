da9a41a8daca7de89b64df82d0079cdcfce9821b
Gil Lacerda <gilclacerda@gmail.com>
2017-01-27 23:31:03 +0000

    Correct usages of Department's getName method to account for localization Replace usages of Department's getNameI18N, getRealName and getRealNameEn methods with the getFullName method

--- fenixedu-ist-teacher-evaluation/src/main/java/pt/ist/fenixedu/teacher/evaluation/ui/struts/action/teacher/evaluation/TeacherEvaluationDA.java
+++ fenixedu-ist-teacher-evaluation/src/main/java/pt/ist/fenixedu/teacher/evaluation/ui/struts/action/teacher/evaluation/TeacherEvaluationDA.java
@@ -461,7 +461,8 @@ public class TeacherEvaluationDA extends FenixDispatchAction {
                     if (teacherEvaluation != null) {
                         String department =
                                 teacherEvaluation.getTeacherEvaluationProcess().getEvaluee().getTeacher().getLastDepartment() == null ? withoutDepartment : teacherEvaluation
-                                        .getTeacherEvaluationProcess().getEvaluee().getTeacher().getLastDepartment().getName();
+                                        .getTeacherEvaluationProcess().getEvaluee().getTeacher().getLastDepartment().getName()
+                                        .getContent();
                         for (TeacherEvaluationFileType teacherEvaluationFileType : teacherEvaluation.getAutoEvaluationFileSet()) {
                             TeacherEvaluationFileBean teacherEvaluationFileBean =
                                     new TeacherEvaluationFileBean(teacherEvaluation, teacherEvaluationFileType);
@@ -508,7 +509,7 @@ public class TeacherEvaluationDA extends FenixDispatchAction {
                     if (teacherEvaluation != null) {
                         String department =
                                 teacherEvaluation.getTeacherEvaluationProcess().getEvaluee().getTeacher().getLastDepartment() == null ? withoutDepartment : teacherEvaluation
-                                        .getTeacherEvaluationProcess().getEvaluee().getTeacher().getLastDepartment().getName();
+                                        .getTeacherEvaluationProcess().getEvaluee().getTeacher().getLastDepartment().getName().getContent();
                         for (TeacherEvaluationFileType teacherEvaluationFileType : teacherEvaluation.getEvaluationFileSet()) {
                             TeacherEvaluationFileBean teacherEvaluationFileBean =
                                     new TeacherEvaluationFileBean(teacherEvaluation, teacherEvaluationFileType);
